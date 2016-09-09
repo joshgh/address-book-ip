5aaaec1c5a3051b956d41e2b44e25aeefe14a4c1
Joshua Huffman <j.m.huffman@gmail.com>
2016-09-09 09:51:21 -0700

    create template for home, create custom stylesheet with styling for form

--- app/app.php
+++ app/app.php
@@ -2,6 +2,11 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Address.php";
 
+    session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
