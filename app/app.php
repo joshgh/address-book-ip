94466068bb83347db2cbcdeba53e2d4617c3c972
Joshua Huffman <j.m.huffman@gmail.com>
2016-09-09 09:23:27 -0700

    link twig in app.php, create routes for each action

--- app/app.php
+++ app/app.php
@@ -3,9 +3,18 @@
     require_once __DIR__."/../src/Address.php";
 
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    $app->get("/", function() {
-        return "Home";
+    $app->get("/", function() use($app) {
+        return $app['twig']->render('home.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+    $app->get("/create_contact", function() use($app) {
+        return $app['twig']->render('create_contact.html.twig', array('contact' => $contact));
+    });
+
+    $app->get("/delete_contacts", function() use($app) {
+        return $app['twig']->render('delete_contacts.html.twig');
     });
 
     return $app;
